21b4f1ead9e1d31961a5c52c8c879e9738ada1ba
Merrick Kavolsky <technicmk@gmail.com>
2015-12-11 11:00:08 +0000

    Add some test functions

--- game.py
+++ game.py
@@ -4,8 +4,10 @@ import pygame
 
 from cake.gameobject import GameObject
 from cake.input import EventHandler
+from world import World
 from player import Player
 from enemy import Enemy
+
 
 
 class Game:
@@ -13,22 +15,51 @@ class Game:
         Generic class that handles game setup,
         and game loop as well as in game eventhandlers
     """
-    def __init__(self):
+    def __init__(self, fps=50):
         events = EventHandler()
-        events.assign_keyup(pygame.K_ESCAPE, self.pause)
+        events.assign_keyup(pygame.K_p, self.pause)
+        events.assign_keyup(pygame.K_q, self.quit)
+        events.assign_keyup(pygame.K_f, self.toggle_focus)
+        events.assign_keyup(pygame.K_t, self.toggle_autofocus)
         self.events = events
+        self.fps = fps
+        self.focused = False
         
             
+    def toggle_autofocus(self):
+        p = self.data['game']['player']
+        p.auto_focus = not p.auto_focus
+        print("Autofocus: %s" % p.auto_focus)
+
+    def toggle_focus(self):
+        w = self.data['game']['world']
+        p = self.data['game']['player']
+        if self.focused:
+            w.remove_focus()
+        else:
+            w.set_focus(p, vert=True, offsety=0)
+        self.focused = not self.focused
+            
+
     def initialize(self, data):
         print("initializing...")
-        game_data = {}
-        data['game'] = game_data
-        # put level info outside 'game_data' to allow for
-        # level selection in future menus
+        # put level info outside 'game_data' to allow for level selection in future menus
         data['level'] = 1
+        w = World(data['SCREEN_SIZE'][0]*2, data['SCREEN_SIZE'][1], data['SCREEN_SIZE'], bg_color=(0, 255, 255)) 
+        p = Player(100, 300, w)
+        e = Enemy(10, 300, w)
+        e2 = Enemy(300, 300, w)
+        w.add_player(p) 
+        w.add_enemy(e)
+        w.add_enemy(e2)
+        # w.set_focus(p)
+        game= {}
+        game['particles'] = pygame.sprite.Group()
+        game['spawners'] = pygame.sprite.Group()
+        game['world'] = w
+        game['player'] = p
+        data['game'] = game
         self.data = data
-        self.game_data = game_data
-
 
     def run(self, data):
         if data['game'] is None:
@@ -36,15 +67,28 @@ class Game:
             print("game started")
         else: 
             print("game resumed")
-        game_data = self.game_data
+        game= data['game']
         clock = pygame.time.Clock()
         screen = data['screen']
+        fps = self.fps
+        events = self.events
+        t1 = time.time()
+        particles = game['particles']
+        spawners = game['spawners']
+        world = game['world']
 
         # game loop
         while self.data['in_game']:
-            clock.tick(1)
+            clock.tick(fps)
             screen.fill((148,148,148))
-            self.events.handle_events()
+            events.handle_events()
+
+            t2 = time.time()
+            dt = t2 - t1
+
+            world.update(dt, screen)
+            particles.update(dt, screen)
+            spawners.update(t2)
             pygame.display.flip()
 
 
@@ -54,3 +98,10 @@ class Game:
         self.data['in_pause_menu'] = True
         self.data['in_start_menu'] = False
         print("game paused")
+
+    def quit(self):
+        self.data['in_game'] = False
+        self.data['in_pause_menu'] = False
+        self.data['in_start_menu'] = False
+        print("game quit")
+        
