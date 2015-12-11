3f74377d73565f64e311456115ea8bc2f1dd6622
Merrick Kavolsky <technicmk@gmail.com>
2015-12-11 16:48:53 +0000

    Remove autofocus on player

--- player.py
+++ player.py
@@ -33,40 +33,17 @@ class Player(GameObject):
         
         return self.world.is_move_valid(rect)
 
-    def __auto_focus__(self):
-        """
-            Automatically focus the camera on
-            player appropriately. 
-            Automatically called after player moves
-        """
-        if not self.auto_focus:
-            self.world.focus = None
-            return
-        w = self.world
-        scr_w, scr_h = w.screen_size
-        wrld_w, wrld_h = w.width, w.height
-        if self.position.x < scr_w//2:
-            w.remove_focus()
-        elif self.position.x >= wrld_w - scr_w//2:
-            w.set_focus(self.rect.copy())
-        else:
-            w.set_focus(self)
-
-
-
     def update(self, dt):
         
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_LEFT]:
             if self.is_move_valid(x=-self.speed):
                 self.position.x -= self.speed
-                self.__auto_focus__()
             else:
                 print("left invalid")
         if keystate[pygame.K_RIGHT]:
             if self.is_move_valid(x=self.speed):
                 self.position.x += self.speed
-                self.__auto_focus__()
             else:
                 print("right invalid")
         if keystate[pygame.K_UP]:
