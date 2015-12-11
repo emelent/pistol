ca93e453961c1cfa0700f7ac13a6a1577f473a03
Merrick Kavolsky <technicmk@gmail.com>
2015-12-11 10:56:10 +0000

    Add Enemy class

--- enemy.py
+++ enemy.py
@@ -1,3 +1,11 @@
+import pygame
+from cake.gameobject import GameObject
 
-class Enemy:
-    pass
+class Enemy(GameObject):
+
+    def __init__(self, x, y, world):
+        image = pygame.Surface((50, 50))
+        image.fill((128,0,0))
+        super(Enemy, self).__init__(image)
+        self.set_position(x, y)
+        self.world = world
